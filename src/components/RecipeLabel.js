cb576b938c5782cdfa1a13b1292bf297b185e3c4
Dave Lange <dave.langesilva@gmail.com>
2021-02-10 22:08:32 +0000

    Fix Label component
    Fix Label component to include css classes

--- src/components/RecipeLabel.js
+++ src/components/RecipeLabel.js
@@ -1,11 +1,7 @@
-function RecipeLabel({color, children}) {
-
-    const baseClass = "text-xs inline-flex items-center rounded-sm px-1 mr-1 mb-1 max-w-5";
-    
-    const colorClass = `${baseClass} bg-${color}-100 text-${color}-500`;
-
+function RecipeLabel({className, children}) {
+        
     return (
-        <div className={colorClass}>
+        <div className={`text-xs inline-flex items-center rounded-sm px-1 mr-1 mb-1 max-w-5 ${className}`}>
             { children }
         </div>
     )
--- src/containers/LabelDisplay.js
+++ src/containers/LabelDisplay.js
@@ -6,13 +6,13 @@ function LabelDisplay({limit, recipe}) {
 
     return (
         <div className="flex flex-wrap">
-            <RecipeLabel color="yellow">
+            <RecipeLabel className="bg-yellow-100 text-yellow-500">
                 <FeatherIcon icon="clock" width="12" height="12" />
                 <span className="ml-1">{ recipeTimeCalc(recipe.readyInMinutes) }</span>
             </RecipeLabel>
 
             { dietList(limit, recipe).map( (item, i) => 
-                <RecipeLabel color="blue" key={i}>
+                <RecipeLabel className="bg-blue-100 text-blue-500" key={i}>
                     {item}
                 </RecipeLabel> )
             } 
